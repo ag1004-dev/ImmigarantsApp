d5c347766633c970f51c8ced9ae58e8f110a4db4
SmartDev116 <williambennett116ok@proton.me>
2018-08-21 21:04:55 -0300

    Added new js file and new pictures used

---
+++ scriptado.js
@@ -0,0 +1,7 @@
+function changeMeme() {
+    document.getElementById("memejs").src = "ibagens/pogchamp.png";
+}
+
+function changeBack() {
+    document.getElementById("memejs").src = "ibagens/resident.png";
+}
