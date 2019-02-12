1078e35b54d579da53d4092ee20da0f1e257e2fb
SmartDev116 <williambennett116ok@proton.me>
2019-02-12 01:50:58 -0200

    Added sidebar and more JS examples

--- scriptado.js
+++ scriptado.js
@@ -1,3 +1,33 @@
+var listElement = document.getElementById("lista");
+var buttonElement = document.getElementById("add");
+var inputElement = document.getElementById("input_todo");
+
+var todos = [
+];
+
+function renderTodos(){
+    listElement.innerHTML = '';
+
+    for(todo of todos){
+        var todoElement = document.createElement("li");
+        var todoText = document.createTextNode(todo);
+
+        //Adicionando o novo texto no li todoElement
+        todoElement.appendChild(todoText);
+        //Adicionando o li todoElement na ul listElement
+        listElement.appendChild(todoElement);
+    }
+}
+
+function addTodo(){
+    var todoText = inputElement.value;
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+}
+
+buttonElement.onclick = addTodo;
+
 function changeMeme() {
     document.getElementById("memejs").src = "ibagens/pogchamp.png";
 }
