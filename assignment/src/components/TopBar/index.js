1d6bc4c417d850c70e0a4c6f14db9347c9e8c50d
SmartDev116 <williambennett116ok@proton.me>
2019-04-15 21:20:22 -0300

    homepage almost finished, missing animation only

--- assignment/src/components/TopBar/index.js
+++ assignment/src/components/TopBar/index.js
@@ -1,31 +1,51 @@
-import React from "react";
+import React, {Component} from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { faAddressCard } from "@fortawesome/free-solid-svg-icons";
 import { Header, Navbrand, FaIcon } from "./styles";
+import './styles.css'
 
-const TopBar = () => (
-  <Header>
-    <Navbar
-      defaultActiveKey="/home"
-      fixed="top"
-      id="mainNavbar"
-      variant="light"
-    >
-      <Container>
-        <Navbrand href="/home">
-          <FaIcon icon={faAddressCard} />
-          Immigrant Song
-        </Navbrand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Nav className="justify-content-end">
-          <Nav.Link href="/home" className="active">
-            Home
-          </Nav.Link>
-          <Nav.Link href="#link">Buscador</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
-  </Header>
-);
+
+class TopBar extends Component {
+  state = {
+    isTop: true,
+  };
+
+  componentDidMount() {
+    document.addEventListener('scroll', () => {
+      const isTop = window.scrollY < 100;
+      if (isTop !== this.state.isTop) {
+        this.setState({ isTop })
+      }
+    });
+  }
+  
+  render(){
+    return(
+      <Header>
+        <Navbar
+          defaultActiveKey="/home"
+          fixed="top"
+          id="mainNavbar"
+          variant="light"
+          className={this.state.isTop ? ' ' : 'bg-light'}
+        >
+          <Container>
+            <Navbrand href="/home">
+              <FaIcon icon={faAddressCard} />
+              Immigrant Song
+            </Navbrand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Nav className="justify-content-end">
+              <Nav.Link href="/home" className="active">
+                Home
+              </Nav.Link>
+              <Nav.Link href="#link">Buscador</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </Header>
+    );
+  }
+}
 
 export default TopBar;
